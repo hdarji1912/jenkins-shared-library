fe980ad3c36d82307eb750b23ac833e79dd384df
HARDIK DARJI <hdarji783@gmail.com>
2026-07-10 22:12:00 +0530

    Update Docker deployment script with username

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,28 +1,23 @@
-def call(String imageName = "todo-app",
+def call(String dockerUsername = "darjihardik",
+         String imageName = "todo-app",
          String imageTag = "latest",
          String containerName = "todo-app",
          String hostPort = "5000",
          String containerPort = "5000") {
 
-    
     echo "Deploying Docker Container..."
-  
 
     sh """
-        # Stop the container if it is running
+        docker pull ${dockerUsername}/${imageName}:${imageTag}
+
         docker stop ${containerName} || true
 
-        # Remove the container if it exists
         docker rm ${containerName} || true
 
-        # Remove old image (optional)
-        docker rmi ${imageName}:${imageTag} || true
-
-        # Run the latest container
         docker run -d \
-            --name ${containerName} \
-            -p ${hostPort}:${containerPort} \
-            ${imageName}:${imageTag}
+          --name ${containerName} \
+          -p ${hostPort}:${containerPort} \
+          ${dockerUsername}/${imageName}:${imageTag}
     """
 
     echo "Deployment completed successfully."
